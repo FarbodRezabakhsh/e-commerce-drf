62337abf19b9e3bf9f9085d5ec2a0d5e998ffd96
Farbod Rezabakhsh <farbod.rezabakhsh80@gmail.com>
2025-05-01 13:38:14 +0330

    feat(cart): determine admin.py file for cart models.

--- ecommerce/apps/cart/admin.py
+++ ecommerce/apps/cart/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import CartItem, Cart
+
 
 # Register your models here.
+
+class CartItemInLine(admin.TabularInline):
+    model = CartItem
+    extra = 0
+    readonly_fields = ['unit_price', 'total_price']
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'subtotal', 'grand_total','created_at']
+    inlines = (CartItemInLine,)
+    readonly_fields = ['subtotal', 'shipping_fee', 'grand_total']
