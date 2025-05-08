cf89abb5ecc81d2f381f5d54e99b49d8cccdbe17
Farbod Rezabakhsh <farbod.rezabakhsh80@gmail.com>
2025-05-08 17:22:30 +0330

    feat(checkout): Implement payment process(setup order and payment view)

--- ecommerce/apps/orders/views.py
+++ ecommerce/apps/orders/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404, redirect
+from django.utils import timezone
+from apps.cart.models import Cart
+from apps.payments.models import Payment
+from apps.orders.models import Order, OrderItem
+from apps.payments.zarinpal import request_payment, verify_payment
+from rest_framework import status, permissions
 
 # Create your views here.
+
+class CheckoutView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request):
+        cart = get_object_or_404(Cart, user=request.user)
+        if cart.items().count() == 0:
+            return Response({"Detail":"Cart is empty"},status=status.HTTP_404_NOT_FOUND)
+        order = Order.objects.create(user=request.user, total_price=cart.grand_total)
+        OrderItem.objects.bulk_create([
+            OrderItem(
+                order=order,
+                variant=item.variant,
+                quantity=item.quantity,
+                unit_price=item.unit_price,
+                total_price=item.total_price
+            )
+            for item in cart.items.all()
+        ])
+
+        authority, pay_url = request_payment(
+            amount=order.total_price,
+            description=f"Order {order.id}",
+            email=request.user.email,
+        )
+        Payment.objects.create(order=order, authority=authority)
+        cart.items.all().delete()
+
+        return Response({"pay_url":pay_url}, status=201)
+
+
--- ecommerce/apps/payments/views.py
+++ ecommerce/apps/payments/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import status, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404, redirect
+from .zarinpal import verify_payment
+from apps.orders.models import Order
+from .models import Payment
+from django.utils import timezone
 
 # Create your views here.
+
+class VerifyPaymentView(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request):
+        authority = request.GET.get('authority')
+        status_flag = request.GET.get('Status')
+        payment = get_object_or_404(Payment, authority=authority)
+        order = payment.order
+
+        if status_flag != "OK":
+            payment.status = "nok"
+            order.status = Order.FAILED
+            order.save(), payment.save()
+            return redirect("/payment/fail/")
+
+        ok, ref_id = verify_payment(authority, order.total_price)
+        if ok:
+            payment.status, payment.ref_id, payment.paid_at = "ok", ref_id, timezone.now()
+            order.status = Order.PAID
+        else:
+            payment.status, order.status = "nok", Order.FAILED
+        order.save(), payment.save()
+        return redirect("/payment/success/")
+
+
